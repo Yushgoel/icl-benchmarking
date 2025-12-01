0cf10de7a4e3f87fdadcf6d94c32b9e134a082e4
Ayush Goel <ayushgoel@berkeley.edu>
2025-11-30 20:03:37 -0800

    fixed anisotropic data

--- generate_data.py
+++ generate_data.py
@@ -2,8 +2,11 @@ import numpy as np
 
 np.random.seed(42)
 
-def generate_data(n_models, n_samples, n_features, epsilon_var, x_vars, beta_vars = 1):
-    assert len(x_vars) == n_features
+def generate_data(n_models, n_samples, n_features, epsilon_var, x_vars=1, beta_vars=1):
+    if isinstance(beta_vars, (list, np.ndarray)):
+        assert len(beta_vars) == n_features
+    if isinstance(x_vars, (list, np.ndarray)):
+        assert len(x_vars) == n_features
     X = np.random.randn(n_models, n_samples, n_features) * np.sqrt(x_vars)
     # epsilon = np.random.randn(n_models, n_samples) * np.sqrt(epsilon_var)
 
@@ -26,13 +29,13 @@ def generate_isotropic_data():
     
 def generate_anisotropic_data():
     n_features = 5
-    x_vars = np.array([0.5, 1, 1.5, 1, 1.75])
-    X, y, beta = generate_data(n_models=1_000, n_samples=10, n_features=n_features, epsilon_var=0.1, x_vars=x_vars, beta_vars=1)
+    beta_vars = np.array([0.5, 1, 1.5, 1, 1.75])
+    X, y, beta = generate_data(n_models=1_000, n_samples=10, n_features=n_features, epsilon_var=0.1, x_vars=1, beta_vars=beta_vars)
 
     np.savez('data/anisotropic_data.npz', X=X, y=y, beta=beta)
 
 if __name__ == '__main__':
-    print('Generating isotropic data...')
-    generate_isotropic_data()
+    # print('Generating isotropic data...')
+    # generate_isotropic_data()
     print('Generating anisotropic data...')
     generate_anisotropic_data()
